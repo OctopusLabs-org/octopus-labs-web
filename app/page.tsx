12c2749932d7648facc6e014601e5b335c1b2a43
Ashen Jayarathne <ashenjayarathnedev@gmail.com>
2024-11-15 14:47:48 +0530

    Define some global variables

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <>
+    <div className="">
       <h1>Heading 1</h1>
       <h2>Heading 2</h2>
       <h3>Heading 3</h3>
@@ -13,7 +13,7 @@ const Home = () => {
         voluptatem. Tenetur repudiandae labore, voluptate ut tempore, eaque quae
         rem error aut quo fugit nulla dolor doloremque et minus modi.
       </p>
-    </>
+    </div>
   );
 };
 
