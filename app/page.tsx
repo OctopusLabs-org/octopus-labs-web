4b7a5779c55f74d23e9ccdb173c064445009eb58
Ashen Jayarathne <ashenjayarathnedev@gmail.com>
2024-11-15 14:37:49 +0530

    Define typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quas eaque
+        possimus ratione iure sint dolorum porro! Et doloremque asperiores
+        voluptatem. Tenetur repudiandae labore, voluptate ut tempore, eaque quae
+        rem error aut quo fugit nulla dolor doloremque et minus modi.
+      </p>
+    </>
+  );
 };
 
 export default Home;
