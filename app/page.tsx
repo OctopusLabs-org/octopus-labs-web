f77b176ed4acafd1d8402c229345fd26b9e1feca
Ashen Jayarathne <ashenjayarathnedev@gmail.com>
2024-11-16 08:58:28 +0530

    Implement navbar layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import MainSection from "./components/MainSection";
+import NavBar from "./components/NavBar";
 
 const Home = () => {
   return (
     <div>
+      <NavBar />
       <MainSection />
     </div>
   );
