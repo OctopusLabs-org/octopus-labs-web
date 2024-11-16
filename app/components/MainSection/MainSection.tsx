176b6bcff17e940f29571dd6549c68355dfc92f9
Ashen Jayarathne <ashenjayarathnedev@gmail.com>
2024-11-16 12:01:58 +0530

    Fixed an issue

--- app/components/MainSection/MainSection.tsx
+++ app/components/MainSection/MainSection.tsx
@@ -5,16 +5,16 @@ const MainSection = () => {
   return (
     <div className={[styles.gridContainer, styles.container].join(" ")}>
       <div className={styles.gridItem}>
-        <h1 className={styles.mainHeading}>We're Octopus Labs</h1>
+        <h1 className={styles.mainHeading}>We&apos;re Octopus Labs</h1>
         <span className={styles.mainTitle}>
-          "Creative Solutions, Tailored for Your Needs"
+          &quot;Creative Solutions, Tailored for Your Needs&quot;
         </span>
         <p className={styles.mainDescription}>
           Empowering small to medium businesses and individuals with innovative
           graphic design and web development services. Whether you need stunning
           visuals or robust web applications, our dedicated team delivers
-          solutions that align perfectly with your goals. Let’s bring your ideas
-          to life with creativity and precision!
+          solutions that align perfectly with your goals. Let&apos;s bring your
+          ideas to life with creativity and precision!
         </p>
 
         <div className={styles.btnGroup}>
