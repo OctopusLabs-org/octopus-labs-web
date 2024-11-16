7e7a16f4aa228a250eaebf0277e8309035e368e2
Ashen Jayarathne <ashenjayarathnedev@gmail.com>
2024-11-16 10:02:02 +0530

    Implement services section

---
+++ app/components/ServicesSection/ServicesSection.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./ServicesSection.module.css";
+
+const ServicesSection = () => {
+  return (
+    <div className={[styles.serviceWrapper, styles.container].join(" ")}>
+      <h2>Our Services</h2>
+
+      <div className={styles.serviceGrid}>
+        <div className={styles.gridItem}>
+          <h3 className={styles.heading}>Graphic Designing</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+            commodi dolorem accusamus repudiandae tempore cupiditate impedit
+            quidem ullam atque odit in pariatur recusandae ab odio deserunt
+            repellendus quod aspernatur a qui? Officia aut veritatis, expedita
+            placeat cupiditate in eaque perspiciatis maxime sequi nihil
+            dignissimos asperiores amet vitae ea, debitis quis tempora modi!
+            Numquam consequuntur sunt animi ducimus, incidunt cupiditate
+            dolorem!
+          </p>
+        </div>
+        <div className={styles.gridItem}>
+          <h3 className={styles.heading}>Web Development</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+            commodi dolorem accusamus repudiandae tempore cupiditate impedit
+            quidem ullam atque odit in pariatur recusandae ab odio deserunt
+            repellendus quod aspernatur a qui? Officia aut veritatis, expedita
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MainSection from "./components/MainSection";
 import NavBar from "./components/NavBar";
+import ServicesSection from "./components/ServicesSection";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <NavBar />
       <div className="content-wrapper">
         <MainSection />
+        <ServicesSection />
       </div>
     </div>
   );
