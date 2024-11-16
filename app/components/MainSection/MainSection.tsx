8861c49fe4ecffa1108ba8b9ec9ba91dc381390b
Ashen Jayarathne <ashenjayarathnedev@gmail.com>
2024-11-16 07:35:11 +0530

    Define main content

---
+++ app/components/MainSection/MainSection.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./MainSection.module.css";
+
+const MainSection = () => {
+  return (
+    <div className={[styles.gridContainer, styles.container].join(" ")}>
+      <div className={styles.gridItem}>
+        <h1 className={styles.mainHeading}>We're Octopus Labs</h1>
+        <span className={styles.mainTitle}>
+          "Creative Solutions, Tailored for Your Needs"
+        </span>
+        <p>
+          Empowering small to medium businesses and individuals with innovative
+          graphic design and web development services. Whether you need stunning
+          visuals or robust web applications, our dedicated team delivers
+          solutions that align perfectly with your goals. Let’s bring your ideas
+          to life with creativity and precision!
+        </p>
+      </div>
+
+      <div className={styles.gridItem}>Octopus Logo</div>
+    </div>
+  );
+};
+
+export default MainSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
 import React from "react";
+import MainSection from "./components/MainSection";
 
 const Home = () => {
   return (
-    <div className="">
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quas eaque
-        possimus ratione iure sint dolorum porro! Et doloremque asperiores
-        voluptatem. Tenetur repudiandae labore, voluptate ut tempore, eaque quae
-        rem error aut quo fugit nulla dolor doloremque et minus modi.
-      </p>
+    <div>
+      <MainSection />
     </div>
   );
 };
