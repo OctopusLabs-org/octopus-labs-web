656440e9d4ec525c0030ea3809bc28fc8a000a01
Ashen Jayarathne <ashenjayarathnedev@gmail.com>
2024-11-16 09:40:56 +0530

    Add buttons to main component

--- app/components/MainSection/MainSection.tsx
+++ app/components/MainSection/MainSection.tsx
@@ -10,7 +10,7 @@ const MainSection = () => {
         <span className={styles.mainTitle}>
           "Creative Solutions, Tailored for Your Needs"
         </span>
-        <p>
+        <p className={styles.mainDescription}>
           Empowering small to medium businesses and individuals with innovative
           graphic design and web development services. Whether you need stunning
           visuals or robust web applications, our dedicated team delivers
@@ -18,8 +18,10 @@ const MainSection = () => {
           to life with creativity and precision!
         </p>
 
-        <Button>Contact Us</Button>
-        <Button skin="outline">About Us</Button>
+        <div className={styles.btnGroup}>
+          <Button>Contact Us</Button>
+          <Button skin="outline">About Us</Button>
+        </div>
       </div>
 
       <div className={styles.gridItem}>Octopus Logo</div>
